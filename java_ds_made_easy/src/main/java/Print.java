5a7c3e028695dbb2f5109f92328a3d3e72017f21
Carmen <carmen.pomana04@gmail.com>
2018-11-17 14:26:20 +0100

    Implemented utility class for writinf in different colors to the console.

---
+++ java_ds_made_easy/src/main/java/Print.java
@@ -0,0 +1,26 @@
+class Print {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    
+    static void red(String text) {
+        System.out.print(ANSI_RED + text + ANSI_RESET);
+    }
+    
+    static void green (String text) {
+        System.out.print(ANSI_GREEN + text + ANSI_RESET);
+    }
+
+    public static void main(String[] args) {
+        Print.red("RED");
+        System.out.println();
+        Print.green("GREEN");
+    }
+}
+
